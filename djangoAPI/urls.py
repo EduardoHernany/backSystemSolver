be4877941ee51ecb4f71a2405037710558ab9f9a
EduardoHernany <eduardohernany.pdm@gmail.com>
2023-09-25 01:24:20 -0400

    SystemSolver backend 24/7

--- djangoAPI/urls.py
+++ djangoAPI/urls.py
@@ -6,6 +6,7 @@ from systemsolver import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/matrix/triangularize/', views.triangularize_matrix, name='triangularize-matrix'),
+    path('api/matrix/elimination_gaussiana/', views.elimination_gaussiana, name='elimination_gaussiana'),
 
 ]
 
--- systemsolver/views.py
+++ systemsolver/views.py
@@ -49,14 +49,23 @@ def triangularize_matrix(request):
             matrix = data.get('data')
 
             if matrix:
-                
+                matrix = np.array(matrix)
+
+                b = matrix[:, -1]
+                # Separe 'A' do restante da matriz
+                A = matrix[:, :-1]
+
+                A = A.astype('float64')
+                b = b.astype('float64')
+                x, A, b,steps= elimination_gaussianaa(A,b)
+
+                # Concatene 'A' e 'b' horizontalmente
+                augmented_matrix = np.column_stack((A, b))
 
 
-                # Suponha que você queira apenas retornar a matriz original por enquanto
-                result = triangularize_matrixx(matrix)
 
-                # Retorne a matriz de resultado como JSON
-                return JsonResponse({'result': result})
+                # Retorne a matriz como JSON
+                return JsonResponse({'result': x.tolist(),'matrix': augmented_matrix.tolist(),'pasos':steps})
             else:
                 return JsonResponse({'error': 'Dados de matriz ausentes ou em formato inválido.'}, status=400)
         except json.JSONDecodeError:
@@ -64,32 +73,75 @@ def triangularize_matrix(request):
     else:
         return JsonResponse({'error': 'Método não permitido'}, status=405)
 
-
-def gaussian_elimination(matrix):
-    # Converter a matriz para um formato numpy
-    matrix = np.array(matrix, dtype=float)
-
-    n, m = matrix.shape
-    if n + 1 != m:
-        raise ValueError("A matriz deve ser do formato nxn+1 para representar um sistema linear.")
-
-    # Eliminação Gaussiana
-    for i in range(n):
-        # Pivoteamento parcial (trocar linhas se necessário)
-        max_row = i
-        for k in range(i + 1, n):
-            if abs(matrix[k, i]) > abs(matrix[max_row, i]):
-                max_row = k
-        matrix[[i, max_row]] = matrix[[max_row, i]]
-
-        # Escalonar a linha atual
-        for j in range(i + 1, n):
-            ratio = matrix[j, i] / matrix[i, i]
-            matrix[j, i:] -= ratio * matrix[i, i:]
-
-    # Resolução de retrosubstituição
+def elimination_gaussianaa(A, b):
+    n = len(b)
     x = np.zeros(n)
-    for i in range(n - 1, -1, -1):
-        x[i] = (matrix[i, -1] - np.dot(matrix[i, i+1:n], x[i+1:])) / matrix[i, i]
+    steps = ""
 
-    return x.tolist()
+    # Fase de eliminação
+    for i in range(n):
+        pivot = A[i, i]
+        if pivot == 0:
+            return None  # Verificar divisão por zero
+        
+        # Adicione o passo à string
+        steps += f"Passo {i + 1}:\n"
+        steps += f"Pivot = {pivot}\n"
+        steps += f"Dividir a linha {i + 1} por {pivot}\n"
+
+        for j in range(i + 1, n):
+            factor = A[j, i] / pivot
+            steps += f"Subtrair {factor:.2f} vezes a linha {i + 1} da linha {j + 1}\n"
+
+            b[j] -= factor * b[i]
+            A[j, i:] -= factor * A[i, i:]
+    
+    # Fase de substituição retroativa
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+
+    return x, A, b,steps
+
+def elimination_gaussiana(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            matrix_a_data = data.get('matrixA')
+            vector_b_data = data.get('vectorB')
+
+            # Converter os dados da matriz 'A' e do vetor 'b' em arrays NumPy
+            matrix_a = np.array(matrix_a_data, dtype=float)
+            vector_b = np.array(vector_b_data, dtype=float)
+
+            n = len(vector_b)
+
+            # Etapa de Eliminação
+            for pivot_row in range(n):
+                # Encontre o pivô (o elemento principal da linha)
+                pivot = matrix_a[pivot_row][pivot_row]
+
+                # Divida a linha do pivô pelo seu valor para fazer o pivô igual a 1
+                matrix_a[pivot_row] /= pivot
+                vector_b[pivot_row] /= pivot
+
+                # Subtraia a linha do pivô das linhas abaixo dela para zerar os elementos abaixo do pivô
+                for row in range(pivot_row + 1, n):
+                    factor = matrix_a[row][pivot_row]
+                    matrix_a[row] -= factor * matrix_a[pivot_row]
+                    vector_b[row] -= factor * vector_b[pivot_row]
+
+            # Etapa de Substituição Retroativa
+            x = np.zeros(n)
+            for i in range(n - 1, -1, -1):
+                x[i] = vector_b[i]
+                for j in range(i + 1, n):
+                    x[i] -= matrix_a[i][j] * x[j]
+
+            # Retorne o resultado
+            return JsonResponse({'result': x.tolist()})
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Falha ao analisar os dados JSON.'}, status=400)
+    else:
+        return JsonResponse({'error': 'Método não permitido'}, status=405)
