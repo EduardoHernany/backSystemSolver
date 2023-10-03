dc9af70ce495fb42fb73cc7d8329a97252af58d9
EduardoHernany <eduardohernany.pdm@gmail.com>
2023-10-02 22:03:32 -0400

    2/10 jordan

--- djangoAPI/urls.py
+++ djangoAPI/urls.py
@@ -5,8 +5,10 @@ from systemsolver import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/matrix/triangularize/', views.triangularize_matrix, name='triangularize-matrix'),
-    path('api/matrix/elimination_gaussiana/', views.elimination_gaussiana, name='elimination_gaussiana'),
+    path('api/matrix/elimination_gaussiana/', views.jordan_matrix_matrix, name='triangularize-matrix'),
+    #path('api/matrix/elimination_gaussiana/', views.elimination_gaussiana, name='elimination_gaussiana'),
+
+    path('api/matrix/elimination_jordan/', views.jordan_matrix, name='elimination_jordan'),
 
 ]
 
--- systemsolver/views.py
+++ systemsolver/views.py
@@ -7,42 +7,124 @@ from .serializers import MatrixSerializer
 import numpy as np
 
 
-def triangularize_matrixx(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    for i in range(rows):
-        # Encontre a primeira linha com um valor não zero na coluna atual
-        pivot_row = i
-        while pivot_row < rows and matrix[pivot_row][i] == 0:
-            pivot_row += 1
-
-        # Se não encontrarmos uma linha com valor não zero, pule para a próxima coluna
-        if pivot_row == rows:
-            continue
-
-        # Troque as linhas para mover o pivô para a posição atual
-        matrix[i], matrix[pivot_row] = matrix[pivot_row], matrix[i]
-
-        # Normalize a linha atual para que o elemento de pivô seja 1
-        pivot_element = matrix[i][i]
-        for j in range(cols):
-            matrix[i][j] /= pivot_element
-
-        # Elimine os elementos abaixo do pivô
-        for k in range(i + 1, rows):
-            factor = matrix[k][i]
-            for j in range(cols):
-                matrix[k][j] -= factor * matrix[i][j]
-
-    return matrix
-
-# django_app/views.py
 
 from django.http import JsonResponse
 import json
 
-def triangularize_matrix(request):
+def jordan_matrix_matrix(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            matrix = data.get('data')
+
+            if matrix:
+                matrix = np.array(matrix)
+
+                b = matrix[:, -1]
+                # Separe 'A' do restante da matriz
+                A = matrix[:, :-1]
+
+                M= matrix.astype('float64')
+
+                x, M, steps, matrices = elimination_gaussianaa(M)
+
+                
+                matrices_as_list_of_lists = [matrix.tolist() for matrix in matrices]
+
+                # Retorne a matriz como JSON
+                return JsonResponse({'result': x.tolist(), 'matrix': M.tolist(), 'steps': steps, 'matrices': matrices_as_list_of_lists})
+            else:
+                return JsonResponse({'error': 'Dados de matriz ausentes ou em formato inválido.'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Falha ao analisar os dados JSON.'}, status=400)
+    else:
+        return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+def elimination_gaussianaa(A, b):
+    n = len(b)
+    x = np.zeros(n)
+
+    matrices = []
+    steps = []
+
+    # Fase de eliminação
+    for i in range(n):
+        pivot = A[i, i]
+        if pivot == 0:
+            return None  # Verificar divisão por zero
+        
+        for j in range(i + 1, n):
+            lj = np.column_stack((A.copy(), b.copy()))[j, :].copy()  # Copie a linha lj antes de fazer a operação
+            matrices.append(np.column_stack((A.copy(), b.copy())))
+            factor = A[j, i] / pivot
+            f = f'A({j+1},{i+1})/pivô  = {A[j,i]}/{pivot} = {factor}'
+            b[j] -= factor * b[i]
+            A[j, i:] -= factor * A[i, i:]
+           
+            step = {
+                'pivo': pivot,
+                'factor': f,
+                'step_number': len(matrices),
+                'operation': f"L{j + 1} =  {factor:.2f} * L{i + 1}= {np.column_stack((A.copy(), b.copy()))[i,:].tolist()} - L{j + 1}= {lj.tolist()}".replace(',',''),
+            }
+            steps.append(step)
+
+    matrices.append(np.column_stack((A.copy(), b.copy())))
+    # Fase de substituição retroativa
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+
+    return x, A, b, steps, matrices
+
+    
+
+#-----------------------------------Jordan-----------------------------
+
+def jordan_elimination(A, b):
+    n = len(b)
+    x = np.zeros(n)
+
+    matrices = []
+    steps = []
+
+    # Fase de eliminação
+    for i in range(n):
+        pivot = A[i, i]
+        if pivot == 0:
+            return None  # Verificar divisão por zero
+        
+        for j in range(n):
+            if i != j:
+                factor = A[j, i] / pivot
+                f = f'A({j+1},{i+1})/pivô  = {A[j,i]}/{pivot} = {factor}'
+                b[j] -= factor * b[i]
+                A[j, i:] -= factor * A[i, i:]
+                print( A[j, i:])
+                
+                step = {
+                    'pivo': pivot,
+                    'factor': f,
+                    'step_number': len(matrices),
+                    'operation': f"L{j + 1} =  {factor:.2f} * L{j + 1} - L{i + 1}",
+                }
+                steps.append(step)
+
+        matrices.append(np.column_stack((A.copy(), b.copy())))
+
+    # Fase de normalização
+    for i in range(n):
+        factor = A[i, i]
+        b[i] /= factor
+        A[i, :] /= factor
+
+    matrices.append(np.column_stack((A.copy(), b.copy())))
+
+    return b, A, steps, matrices
+
+
+
+
+def jordan_matrix(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
@@ -57,90 +139,18 @@ def triangularize_matrix(request):
 
                 A = A.astype('float64')
                 b = b.astype('float64')
-                x, A, b,steps= elimination_gaussianaa(A,b)
+                x, A, steps, matrices = jordan_elimination(A, b)
 
-                # Concatene 'A' e 'b' horizontalmente
+                # Arredonde os elementos da matriz aumentada com duas casas decimais
                 augmented_matrix = np.column_stack((A, b))
+                augmented_matrix = np.round(augmented_matrix, 2)
 
-
+                matrices_as_list_of_lists = [matrix.tolist() for matrix in matrices]
 
                 # Retorne a matriz como JSON
-                return JsonResponse({'result': x.tolist(),'matrix': augmented_matrix.tolist(),'pasos':steps})
+                return JsonResponse({'result': x.tolist(), 'matrix': augmented_matrix.tolist(), 'steps': steps, 'matrices': matrices_as_list_of_lists})
             else:
                 return JsonResponse({'error': 'Dados de matriz ausentes ou em formato inválido.'}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Falha ao analisar os dados JSON.'}, status=400)
-    else:
-        return JsonResponse({'error': 'Método não permitido'}, status=405)
-
-def elimination_gaussianaa(A, b):
-    n = len(b)
-    x = np.zeros(n)
-    steps = ""
-
-    # Fase de eliminação
-    for i in range(n):
-        pivot = A[i, i]
-        if pivot == 0:
-            return None  # Verificar divisão por zero
-        
-        # Adicione o passo à string
-        steps += f"Passo {i + 1}:\n"
-        steps += f"Pivot = {pivot}\n"
-        steps += f"Dividir a linha {i + 1} por {pivot}\n"
-
-        for j in range(i + 1, n):
-            factor = A[j, i] / pivot
-            steps += f"Subtrair {factor:.2f} vezes a linha {i + 1} da linha {j + 1}\n"
-
-            b[j] -= factor * b[i]
-            A[j, i:] -= factor * A[i, i:]
-    
-    # Fase de substituição retroativa
-    for i in range(n - 1, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
-
-    return x, A, b,steps
-
-def elimination_gaussiana(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-
-            matrix_a_data = data.get('matrixA')
-            vector_b_data = data.get('vectorB')
-
-            # Converter os dados da matriz 'A' e do vetor 'b' em arrays NumPy
-            matrix_a = np.array(matrix_a_data, dtype=float)
-            vector_b = np.array(vector_b_data, dtype=float)
-
-            n = len(vector_b)
-
-            # Etapa de Eliminação
-            for pivot_row in range(n):
-                # Encontre o pivô (o elemento principal da linha)
-                pivot = matrix_a[pivot_row][pivot_row]
-
-                # Divida a linha do pivô pelo seu valor para fazer o pivô igual a 1
-                matrix_a[pivot_row] /= pivot
-                vector_b[pivot_row] /= pivot
-
-                # Subtraia a linha do pivô das linhas abaixo dela para zerar os elementos abaixo do pivô
-                for row in range(pivot_row + 1, n):
-                    factor = matrix_a[row][pivot_row]
-                    matrix_a[row] -= factor * matrix_a[pivot_row]
-                    vector_b[row] -= factor * vector_b[pivot_row]
-
-            # Etapa de Substituição Retroativa
-            x = np.zeros(n)
-            for i in range(n - 1, -1, -1):
-                x[i] = vector_b[i]
-                for j in range(i + 1, n):
-                    x[i] -= matrix_a[i][j] * x[j]
-
-            # Retorne o resultado
-            return JsonResponse({'result': x.tolist()})
-
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Falha ao analisar os dados JSON.'}, status=400)
     else:
